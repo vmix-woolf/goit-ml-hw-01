6b1cb4e5e80b0dc91905150c413880508a23f19e
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-05-10 21:30:25 +0300

    load california dataset

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import pandas as pd
+#%%
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+#%%
+california = fetch_california_housing(as_frame=True)
+df = california.frame
+
+print(df.head())
+print(df.info())
+print(df.describe())
